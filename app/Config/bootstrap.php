c1839def1941998267d86fcf61b1d7025e9a397d
yasin-buet <yasin.buet@gmail.com>
2016-02-10 10:01:44 +0600

    53718_embed_vimeo_video

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -24,6 +24,8 @@
 
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
+CakePlugin::load('VideoEmbed');
+
 
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
